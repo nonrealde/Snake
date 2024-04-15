44f5d91be675d22a5fda6d8d45bcd17182b6f93f
Daniel <daniel@bender.kiwi>
2024-04-15 11:45:54 +0200

    much bullshit

--- src/Gui.java
+++ src/Gui.java
@@ -1,25 +1,34 @@
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
 public class Gui extends JFrame {
    
-    private static JFrame f = new JFrame("Test");
-    private static JFrame b = new JFrame();
+    private static JFrame frame = new JFrame("Test");
+    private static JPanel board = new JPanel();
     private static JButton b1 = new JButton("Button1");
     private static JButton b2 = new JButton("Button2");
 
     
     public static void settings() {
-        f.setSize(800,800);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setVisible(true);
-        f.setLocationRelativeTo(null);
+        frame.setSize(1200,800);
+        frame.setLayout(new GridLayout());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
     }
 
     public static void addButtons() {
-        f.add(b1, java.awt.BorderLayout.PAGE_START);
-        f.add(b2, java.awt.BorderLayout.PAGE_END);
-        // f.add(b, java.awt.BorderLayout.CENTER);
-        b.setLocation(200,200);
-        f.pack();
+        frame.add(b1);
+        frame.add(b2);
+        // board.setLocation(200,200);
+        frame.add(board);
+        // board.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        board.setSize(new Dimension(200,200));
+        board.setBackground(Color.BLUE);
+        // frame.pack();
     }    
 }
