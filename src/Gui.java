fb595663107cc9a55889b6ef5c06a145ccae8198
Daniel <daniel@bender.kiwi>
2024-04-15 12:00:00 +0200

    base layout done?

--- src/Gui.java
+++ src/Gui.java
@@ -4,7 +4,8 @@ import javax.swing.JPanel;
 
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.GridLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 public class Gui extends JFrame {
    
     private static JFrame frame = new JFrame("Test");
@@ -14,21 +15,41 @@ public class Gui extends JFrame {
 
     
     public static void settings() {
-        frame.setSize(1200,800);
-        frame.setLayout(new GridLayout());
+        // frame.setSize(1200,800);
+        frame.setLayout(new GridBagLayout());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
         frame.setLocationRelativeTo(null);
-    }
 
-    public static void addButtons() {
-        frame.add(b1);
-        frame.add(b2);
+        GridBagConstraints g = new GridBagConstraints();
+
+        g.fill = GridBagConstraints.HORIZONTAL;
+        g.weightx = 0.5;
+        g.gridx = 0;
+        g.gridy = 0;
+        frame.add(b1, g);
+        g.fill = GridBagConstraints.HORIZONTAL;
+        g.weightx = 0.5;
+        g.gridx = 1;
+        g.gridy = 0;
+        frame.add(b2, g);
         // board.setLocation(200,200);
-        frame.add(board);
+        // grid.fill = GridBagConstraints.HORIZONTAL;
+        g.gridx = 0;
+        g.gridy = 1;
+        g.gridwidth = 2;
+        g.ipadx = 600;
+        g.ipady = 400;
+        g.weightx = 2;
+        frame.add(board, g);
         // board.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
         board.setSize(new Dimension(200,200));
         board.setBackground(Color.BLUE);
-        // frame.pack();
+        frame.pack();
+    }
+
+    public static void addButtons() {
+
+        
     }    
 }
