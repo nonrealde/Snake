d0e7db5173e054b3bffffe530c2f6739428b7e9a
Daniel <daniel@bender.kiwi>
2024-04-15 11:11:56 +0200

    empty

--- src/Gui.java
+++ src/Gui.java
@@ -1,8 +1,25 @@
+import javax.swing.JButton;
 import javax.swing.JFrame;
-public class Gui {
-    public static void mainGui() {
-        JFrame mainFrame = new JFrame("Test");
-        mainFrame.setSize(200,200);
-        mainFrame.setVisible(true);
+public class Gui extends JFrame {
+   
+    private static JFrame f = new JFrame("Test");
+    private static JFrame b = new JFrame();
+    private static JButton b1 = new JButton("Button1");
+    private static JButton b2 = new JButton("Button2");
+
+    
+    public static void settings() {
+        f.setSize(800,800);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+        f.setLocationRelativeTo(null);
     }
+
+    public static void addButtons() {
+        f.add(b1, java.awt.BorderLayout.PAGE_START);
+        f.add(b2, java.awt.BorderLayout.PAGE_END);
+        // f.add(b, java.awt.BorderLayout.CENTER);
+        b.setLocation(200,200);
+        f.pack();
+    }    
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Gui.mainGui();
+        Gui.settings();
+        Gui.addButtons();
     }
 }
