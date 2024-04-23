f162336a64127420a4cde469e03a2bf45ed5701f
Daniel <github@bender.kiwi>
2024-04-23 17:56:28 +0200

    added array for snake body

--- src/Snake.java
+++ src/Snake.java
@@ -2,4 +2,6 @@ public class Snake {
     public static Game.Direction currentDirection = Game.Direction.RIGHT;
     public static int headX;
     public static int headY;
+    public static int length = 0;
+    public static int[][] body = new int[0][2];
 }
