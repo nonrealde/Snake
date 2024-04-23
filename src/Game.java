13f97a004442fe21835d3450f4715a00b37457ca
Daniel <github@bender.kiwi>
2024-04-23 17:55:29 +0200

    created collision method for walls and apple

--- src/Game.java
+++ src/Game.java
@@ -1,13 +1,11 @@
 import java.util.Random;
 
 public class Game {
-    private final int ALLE = Gui.GAME_HEIGHT * Gui.GAME_WIDTH;
-    private final int x[] = new int[ALLE];
-    private final int y[] = new int[ALLE];
     public static int appleX;
     public static int appleY;
 
     static boolean gameRunning = true;
+    static boolean gameOver = false;
     static int FPS = 10;
     Random random = new Random();
 
@@ -71,11 +69,35 @@ public class Game {
     }
 
     public static void checkCollision() {
+        // Snake -> Wand
+        if (Snake.headX < 0 || Snake.headX > Gui.GAME_WIDTH) {
+            gameOver = true;
+        }
+        if (Snake.headY < 0 || Snake.headY > Gui.GAME_HEIGHT) {
+            gameOver = true;
+        }
+        // Snake -> Snake
+        if (Snake.headX == appleX && Snake.headY == appleY) {
+            // temp array old snake state
+            int tmp[][] = Snake.body.clone();
+            // create new array -> new length
+            Snake.body = new int[++Snake.length][2];
+
+            Snake.body[0][0] = appleX;
+            Snake.body[0][1] = appleY;
+
+            // loop over tmp array to fill new array with old values
+            for (int i = 0; i < tmp.length; i++) {
+                Snake.body[i + 1][0] = tmp[i][0];
+                Snake.body[i + 1][1] = tmp[i][1];
+            }
+            
+            System.out.println(Snake.body[0][0] + "|" + Snake.body[0][1]);
+            spawnApple();
+        }
+        // Snake -> APfel
         
     }
-    // Snake -> Wand
-    // Snake -> Snake
-    // Snake -> APfel
 
     public static void gameLoop() {
         double drawInterval = 1000000000/FPS;
@@ -91,8 +113,14 @@ public class Game {
 
             if(delta >= 1) {
                 updateSnakeLocation();
+                checkCollision();
+                if (gameOver) {
+                    //  gameOver();
+                    break;
+                }
                 Gui.refreshFrame();
-                System.out.println(Snake.currentDirection);
+                // System.out.println(Snake.currentDirection + " " + Snake.headX + "|" + Snake.headY);
+                // System.out.println(Snake.body.length);
                 delta--;
             }
         }
