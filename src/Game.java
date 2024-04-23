d14c04ec70d9a4b2ca726826d294b4ff1f732d69
Daniel <github@bender.kiwi>
2024-04-23 12:58:00 +0200

    refactored the declation of some variables and added a simple gameloop based on lastTime run

--- src/Game.java
+++ src/Game.java
@@ -1,20 +1,24 @@
 import javax.swing.Timer;
 
 public class Game {
-    enum Direction {
-        LEFT,
-        TOP,
-        RIGHT,
-        DOWN
-    }
-    public static void initGame() {
-        spawnApple();
-    }
-    private final int ALLE = 900;
+    private final int ALLE = Gui.GAME_HEIGHT * Gui.GAME_WIDTH;
     private final int x[] = new int[ALLE];
     private final int y[] = new int[ALLE];
     public static int appleX;
     public static int appleY;
+    static boolean gameRunning = true;
+    static int FPS = 60;
+    enum Direction {
+        LEFT,
+        UP,
+        RIGHT,
+        DOWN
+    }
+
+    public static void initGame() {
+        spawnApple();
+        gameLoop();
+    }
 
     private Timer t;
 
@@ -23,6 +27,24 @@ public class Game {
         appleY = (int) (Math.random() * Gui.GAME_WIDTH / 10) * 10;
         System.out.println("Apple spawned at: " + appleX + "|" + appleY);
         Gui.refreshFrame();
+    }
+    public static void gameLoop() {
+        double drawInterval = 1000000000/FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
+        while (gameRunning) {
+            // System.out.println(delta);
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime) / drawInterval;
+            lastTime = currentTime;
+
+            if(delta >= 1) {
+            System.out.println(Snake.currentDirection);
+            delta--;
+            }
+        }
     }
     
 }
