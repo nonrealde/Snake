75495edb11f4df4683576fe7133e762386bffd07
Daniel <github@bender.kiwi>
2024-04-23 17:12:20 +0200

    generate start location of snake and added a function to update snake location based on direction

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,4 @@
-import javax.swing.Timer;
+import java.util.Random;
 
 public class Game {
     private final int ALLE = Gui.GAME_HEIGHT * Gui.GAME_WIDTH;
@@ -6,8 +6,11 @@ public class Game {
     private final int y[] = new int[ALLE];
     public static int appleX;
     public static int appleY;
+
     static boolean gameRunning = true;
-    static int FPS = 60;
+    static int FPS = 10;
+    Random random = new Random();
+
     enum Direction {
         LEFT,
         UP,
@@ -16,18 +19,64 @@ public class Game {
     }
 
     public static void initGame() {
+        startingLocation();
         spawnApple();
         gameLoop();
     }
 
-    private Timer t;
-
     public static void spawnApple() {
         appleX = (int) (Math.random() * Gui.GAME_HEIGHT / 10) * 10;
         appleY = (int) (Math.random() * Gui.GAME_WIDTH / 10) * 10;
+
+        // Check that apple doesnt spawn in snake
+        // if(location != snake) {
+        //     lets go
+        // } else {
+        //     neustart methode
+        // }
+
         System.out.println("Apple spawned at: " + appleX + "|" + appleY);
         Gui.refreshFrame();
     }
+
+    public static void startingLocation() {
+        Snake.headX = (int) (Math.random() * (Gui.GAME_HEIGHT - 100) / 10) * 10 + 50;
+        Snake.headY = (int) (Math.random() * (Gui.GAME_WIDTH - 100) / 10) * 10 + 50;
+
+        System.out.println("Snake spawned at: " + Snake.headX + "|" + Snake.headY);
+        Gui.refreshFrame();
+    }
+
+    public static void updateSnakeLocation() {
+        if(Snake.currentDirection == Direction.UP) {
+
+        }
+        switch (Snake.currentDirection) {
+            case Direction.UP:
+                Snake.headY -= 10;
+                break;
+            case Direction.RIGHT:
+                Snake.headX += 10;
+                break;
+            case Direction.DOWN:
+                Snake.headY += 10;
+                break;
+            case Direction.LEFT:
+                Snake.headX -= 10;
+                break;
+            
+            default:
+                break;
+        }
+    }
+
+    public static void checkCollision() {
+        
+    }
+    // Snake -> Wand
+    // Snake -> Snake
+    // Snake -> APfel
+
     public static void gameLoop() {
         double drawInterval = 1000000000/FPS;
         double delta = 0;
@@ -41,8 +90,10 @@ public class Game {
             lastTime = currentTime;
 
             if(delta >= 1) {
-            System.out.println(Snake.currentDirection);
-            delta--;
+                updateSnakeLocation();
+                Gui.refreshFrame();
+                System.out.println(Snake.currentDirection);
+                delta--;
             }
         }
     }
