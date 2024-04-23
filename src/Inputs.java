665e4f82e0eca7693c502e5ae61a12150e0a58da
Daniel <github@bender.kiwi>
2024-04-23 12:59:08 +0200

    added keyBindings for the movement keys (up, right, down, left)

--- src/Inputs.java
+++ src/Inputs.java
@@ -1,6 +1,60 @@
+import java.awt.event.ActionEvent;
 import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
+import javax.swing.Action;
+import javax.swing.AbstractAction;
+import javax.swing.ActionMap;
+import javax.swing.InputMap;
+import javax.swing.JPanel;
+import javax.swing.KeyStroke;
 
 public class Inputs extends KeyAdapter {
-   
+    public static void keyBindings() {
+        
+        Action upAction = new AbstractAction() {
+            public void actionPerformed(ActionEvent e) {
+               System.out.println("UP!"); 
+               if(Snake.currentDirection == Game.Direction.LEFT || Snake.currentDirection == Game.Direction.RIGHT)
+               Snake.currentDirection = Game.Direction.UP;
+            }
+        };
+
+        Action downAction = new AbstractAction() {
+            public void actionPerformed(ActionEvent e) {
+               System.out.println("DOWN!"); 
+               if(Snake.currentDirection == Game.Direction.LEFT || Snake.currentDirection == Game.Direction.RIGHT)
+               Snake.currentDirection = Game.Direction.DOWN;
+            }
+        };
+
+        Action rightAction = new AbstractAction() {
+            public void actionPerformed(ActionEvent e) {
+               System.out.println("RIGHT!"); 
+               if(Snake.currentDirection == Game.Direction.UP || Snake.currentDirection == Game.Direction.DOWN)
+               Snake.currentDirection = Game.Direction.RIGHT;
+            }
+        };
+
+        Action leftAction = new AbstractAction() {
+            public void actionPerformed(ActionEvent e) {
+               System.out.println("LEFT!"); 
+               if(Snake.currentDirection == Game.Direction.UP || Snake.currentDirection == Game.Direction.DOWN)
+               Snake.currentDirection = Game.Direction.LEFT;
+            }
+        };
+
+        InputMap inputMap = Gui.gameboard.getInputMap(JPanel.WHEN_IN_FOCUSED_WINDOW);
+        ActionMap actionMap = Gui.gameboard.getActionMap();
+
+        inputMap.put(KeyStroke.getKeyStroke(38, 0), "upAction");
+        actionMap.put("upAction", upAction);
+
+        inputMap.put(KeyStroke.getKeyStroke(40, 0), "downAction");
+        actionMap.put("downAction", downAction);
+
+        inputMap.put(KeyStroke.getKeyStroke(39, 0), "rightAction");
+        actionMap.put("rightAction", rightAction);
+
+        inputMap.put(KeyStroke.getKeyStroke(37, 0), "leftAction");
+        actionMap.put("leftAction", leftAction);
+   }
 }
